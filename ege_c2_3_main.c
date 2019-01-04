bb230c6fc81297571ccde0136cdbff809f39a787
SMLASH <44621364+SMLASH@users.noreply.github.com>
2019-01-04 23:12:35 +0300

    Add files via upload
    Second commit, ege_c2_3_main.c added

---
+++ ege_c2_3_main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define N 30
+int main(void) {
+    int a[N];
+    int i, j, max;
+    for (i = 0; i < N; i++){
+        scanf("%d", &a[i]);
+    }
+    for(int i = 0; i < N; i++){
+        if(a[i] > max && a[i] < 0){
+            max = a[i];
+        }
+    }
+    printf("%d", max);
+
+}
