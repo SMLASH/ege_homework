7f9583abdeefe95f4aeba2ad8b38de20bcc7f6ed
makar <makaroshkvarkov@mail.ru>
2019-01-05 17:27:48 +0300

    Fourth commit, ege_c4_2_main.c modified

--- ege_c4_2_main.c
+++ ege_c4_2_main.c
@@ -3,6 +3,28 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int n;
+    scanf("%d", &n);
+    int chisla[n];
+    int contrl;
+    for(int i  = 0; i < n; i++){
+        scanf("%d", &chisla[i]);
+    }
+    scanf("%d", &contrl);
+    int r = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(chisla[i] * chisla[j] > r && chisla[i] * chisla[j] % 26 == 0){
+                r = chisla[i] * chisla[j];
+            }
+        }
+    }
+    if(r == contrl){
+        printf("Вычисленное контрольное значение:%d", r);
+    }
+    else{
+        printf("Контроль не пройден");
+    }
+
     return 0;
 }
